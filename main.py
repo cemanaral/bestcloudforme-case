dc41566aaaf830e18cc674cca4731becc0161410
cemanaral <cemanaral425@gmail.com>
2022-01-31 00:30:30 +0300

    adjustments for heroku deployment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, json, jsonify, request
 import urllib.request
+import os
+
 
 app = Flask(__name__)
 
@@ -39,4 +41,5 @@ def not_found(e):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
